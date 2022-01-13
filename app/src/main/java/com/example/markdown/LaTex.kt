2d2f50c7a04de6bf57cccc0cc0c1560f050f117f
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2022-01-13 14:23:38 +0300

    Fragment set in nav graph

--- app/src/main/java/com/example/markdown/LaTex.kt
+++ app/src/main/java/com/example/markdown/LaTex.kt
@@ -11,9 +11,8 @@ class LaTex : Fragment() {
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
         return inflater.inflate(R.layout.fragment_la_tex, container, false)
     }
 
-   
+
 }
