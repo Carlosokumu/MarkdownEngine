3a24204840d21c8f826083c17e5aedc2f326a9c4
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2022-01-13 14:57:19 +0300

    Update text

--- app/src/main/java/com/example/markdown/MarkDownActivity.kt
+++ app/src/main/java/com/example/markdown/MarkDownActivity.kt
@@ -1,7 +1,13 @@
 package com.example.markdown
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MarkDownActivity : AppCompatActivity() {
 
@@ -10,5 +16,24 @@ class MarkDownActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mark_down)
+
+
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.html,
+                R.id.navigation_locations,
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        findViewById<BottomNavigationView>(R.id.nav_view).apply {
+            setupWithNavController(navController)
+        }
+
     }
+
+    override fun onSupportNavigateUp() =
+        Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp()
 }
