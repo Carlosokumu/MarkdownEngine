4797d9b071f6b8371f3ee5320d62ed59e481aaa6
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2022-01-13 14:12:07 +0300

    Latext build and clean

---
+++ app/src/main/java/com/example/markdown/LaTex.kt
@@ -0,0 +1,19 @@
+package com.example.markdown
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class LaTex : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_la_tex, container, false)
+    }
+
+   
+}
