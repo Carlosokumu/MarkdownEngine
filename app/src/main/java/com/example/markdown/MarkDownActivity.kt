2c430310b7aac7a81437c1e0da3a384d1cded74b
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2022-01-13 08:10:14 +0300

    Updated Stuff

---
+++ app/src/main/java/com/example/markdown/MarkDownActivity.kt
@@ -0,0 +1,14 @@
+package com.example.markdown
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MarkDownActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mark_down)
+    }
+}
